2f17fd06199184e7ec8246c99ddd58a003f4f607
TrishDan <struligt@gmail.com>
2018-11-19 09:14:05 +0100

    Add heatmap html pivot table file print.

--- LoanReturn-gen.py
+++ LoanReturn-gen.py
@@ -1,12 +1,3 @@
-"""
-Pulls in some loan reports, creates grouped dataframe and pivot tables showing the calculated effective IRR
-of various clients' loans, bucketed by principal amount . Can be modified to also create pivots bucketed by 
-effective term of loan. 
-Adjust constants section as well as Print section, the latter to define name of xls files to print resulting 
-tables to.
-"""
-
-
 import pandas as pd
 import datetime as dt
 import numpy as np
@@ -25,12 +16,35 @@ def II_eq_counts(tobin_series, num_bins):
     return pd.IntervalIndex.from_arrays(lower_bounds, upper_bounds), upper_bounds
 
 
+def color_vals(val, threshl=[0.15, 0.30, 0.50]):
+    """
+    Takes a scalar and returns a string with
+    the css property `'color: red'` for negative
+    strings, black otherwise.
+    """
+    colormap = ['red', 'black', 'blue', 'green']
+    color = colormap[-1]
+    for i, thresh in enumerate(threshl):
+        if val < thresh:
+            color = colormap[i]
+            break
+    return 'color: %s' % color
+
+
+def heatmap(pivt):
+    """shows a heat map for the pivot table supplied"""
+    s = pivt.style.applymap(color_vals)  # returns a style object
+    with open("temp.html", "w") as f:
+        f.write(s.render())
+
+
 if __name__ == '__main__':
 
     # constants:
     pd.set_option('display.expand_frame_repr', False)
-    files = ['./ClosedLoanSummary201511-201611.xls', './EFSClosedLoanSummary201611-201711.xls']
-    removal_names = ['Unnamed', 'PAP#', 'Carproof', 'Lien', 'Invoice']
+    pd.set_eng_float_format(accuracy=2, use_eng_prefix=True)
+    files = ['./EFSClosedLoanSummary201611-201711.xls', './EFSClosedLoanSummary201711-201811.xls']
+    removal_names = ['Unnamed', 'RIN Fees', 'PAP#', 'Carproof', 'Lien', 'Invoice']
     id_col = 'Loan'
 
     # 1. Readin reports
@@ -65,7 +79,7 @@ if __name__ == '__main__':
     # ..... drop dealers that have little history (eg 1 loan o/s only):
 
     # 5a... add missing fees
-    fees = {'SystemFee': 15, 'Recovery': 15, 'Searches': 0, 'Bank charges': 10, 'LienReg': 16}
+    fees = {'Vinlock': 15.50, 'SystemFee': 15, 'Recovery': 15, 'Searches': 0, 'Bank charges': 10, 'LienReg': 16}
 
     # 5. Calculate important metrics
     internal_fees = 15.50 + 15 + 15 + 0 + 10 + 16
@@ -76,7 +90,8 @@ if __name__ == '__main__':
     go = df.groupby(['Dealer'])
     gdf = go.mean()  # df showing means
     #... add column to grouped df showing # of loans per dealer
-    gdf['no. loans in period'] = go.count()['Principal']
+    gdf['no.loans'] = go.count()['Principal']
+    gdf = gdf[gdf['no.loans'] != 1]  # get rid of dealers with only 1 loan in period
     gdf = gdf.sort_values(by='eff_IRR', ascending=True)  # df showing means
     print(gdf.head(10), "\n")
 
@@ -92,14 +107,15 @@ if __name__ == '__main__':
 
     # 8. Print
     dfs_to_print = [gdf, df_piv]
-    resp_names = ['2015-2017-by Dealer', '2015-2017-by Dealer vs PA']
+    resp_names = ['2016-2018-by Dealer', '2016-2018-by Dealer vs PA']
     # ... Create a Pandas Excel writer using XlsxWriter as the engine.
-    writer = pd.ExcelWriter('2015-2017-return-sum.xlsx', engine='xlsxwriter')
+    writer = pd.ExcelWriter('2016-2018-return-sum.xlsx', engine='xlsxwriter')
     # ...write each dataframe to a different worksheet.
     for i, d in enumerate(dfs_to_print):
         d.to_excel(writer, sheet_name=resp_names[i])
     #...commit
     writer.save()
+    heatmap(df_piv)
 
     # 9. Exit
-    print("... Done - bye bye.")
+    print("... Done - bibi.")
